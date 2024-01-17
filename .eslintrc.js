2b23d78e0d233653cc7f18e7467c29201916fee7
bcygswspring168@gmail.com <bcygswspring168@gmail.com>
2024-01-17 20:14:41 +0800

    第一次提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,3 +17,14 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
+
+// .eslintrc.js
+// 参考文档：https://blog.csdn.net/qq_41887214/article/details/132391992
+
+// 下面这段代码表示用eslint报prettier的错误
+// {
+//   "plugins": ["prettier"],
+//   "rules": {
+//     "prettier/prettier": "error"
+//   }
+// }
